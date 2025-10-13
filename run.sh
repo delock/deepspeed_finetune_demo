3b1b61158cb9416096a98de00989c3420c3f45b8
Ma, Guokai <guokai.ma@intel.com>
2025-10-13 15:23:45 +0000

    add config for super offload

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 NUM="${1:-2}"
 MODEL="${2:-Qwen/Qwen2.5-0.5B}"
 CONFIG="${3:-z2_config.json}"
-deepspeed --num_gpus=$NUM --bind_cores_to_rank finetune_llama.py --model_name $MODEL --output_dir output --lr 2e-5 --batch_size 8 --deepspeed_config $CONFIG --num_train_epochs 1 --bench_start 1
+deepspeed --num_gpus=$NUM --bind_cores_to_rank finetune_llama.py --model_name $MODEL --output_dir output --lr 2e-5 --batch_size 8 --deepspeed_config $CONFIG --num_train_epochs 1 --bench_start 4
 
